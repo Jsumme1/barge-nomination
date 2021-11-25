0ed9141ebb6b24029dc7d2fcbfff6c55514c04fd
jendevane <87095302+jendevane@users.noreply.github.com>
2021-11-25 14:12:24 -0600

    added files

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
+const {Model, DataTypes } = require("sequelize");
+const bcrypt = require('bcrypt');
 const sequelize = require("../config/connection");
 
 // create our User model
@@ -13,12 +13,7 @@ class User extends Model {
 // create fields/columns for User model
 User.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
+    
     firstname: {
       type: DataTypes.STRING,
       allowNull: false,
