8bd355b285c7dd1a92d0a5e2019de8bd32fd5cd9
Jsummers1 <julie.summers2000@gmail.com>
2021-11-24 19:57:52 -0600

    deleted extra files in server.js

--- server.js
+++ server.js
@@ -21,12 +21,9 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require("./utils/helpers");
-
-const hbs = exphbs.create({ helpers });
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// Set Handlebars as the default template engine.
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
