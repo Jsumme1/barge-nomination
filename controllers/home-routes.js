cb513151a3224a8c73d513d7680f4277a8f1c493
Jsummers1 <julie.summers2000@gmail.com>
2021-11-28 11:19:42 -0600

    added last 2 routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -81,6 +81,99 @@ router.get("/nomination/:id", (req, res) => {
     });
 });
 
+// get single nom by counterparty
+router.get("/nomination/:counterparty_name", (req, res) => {
+  Nomination.findOne({
+    where: {
+    counterparty_name: req.params.counterparty_name,
+    },
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          "id",
+          "comment_text",
+          "nomination_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbNominationData) => {
+      if (!dbNominationData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      const nom = dbNominationData.get({ plain: true });
+
+      res.render("single-post", {
+        nom,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get single nom by movement date
+router.get("/nomination/:move_date", (req, res) => {
+  Nomination.findOne({
+    where: {
+      move_date: req.params.move_date,
+    },
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          "id",
+          "comment_text",
+          "nomination_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbNominationData) => {
+      if (!dbNominationData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      const nom = dbNominationData.get({ plain: true });
+
+      res.render("single-post", {
+        nom,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
